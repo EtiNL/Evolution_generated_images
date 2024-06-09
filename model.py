e7c05fbdf1bbafab4ed72585a70229816d2172f8
EtiNL <etienne.lanzeray@gmail.com>
2024-06-10 00:08:56 +0200

    modified:   model.py

--- model.py
+++ model.py
@@ -5,15 +5,15 @@ import torch.nn.functional as F
 class DQN_CNN(nn.Module):
     def __init__(self, input_shape, output_dim):
         super(DQN_CNN, self).__init__()
-        self.conv1 = nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.conv1 = nn.Conv2d(input_shape[0], 32, kernel_size=32, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=16, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=8, stride=1)
         
         def conv2d_size_out(size, kernel_size=1, stride=1):
             return (size - (kernel_size - 1) - 1) // stride + 1
 
-        conv_w = conv2d_size_out(conv2d_size_out(conv2d_size_out(input_shape[1], 8, 4), 4, 2), 3, 1)
-        conv_h = conv2d_size_out(conv2d_size_out(conv2d_size_out(input_shape[2], 8, 4), 4, 2), 3, 1)
+        conv_w = conv2d_size_out(conv2d_size_out(conv2d_size_out(input_shape[1], 64, 4), 32, 2), 8, 1)
+        conv_h = conv2d_size_out(conv2d_size_out(conv2d_size_out(input_shape[2], 64, 4), 32, 2), 8, 1)
         linear_input_size = conv_w * conv_h * 64
 
         # Reduce the size of the fully connected layers
@@ -26,5 +26,5 @@ class DQN_CNN(nn.Module):
         x = F.relu(self.conv3(x))
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
         return x
