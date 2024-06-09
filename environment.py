a5f8b4517ff3630f93e2de815ec2840d58295836
EtiNL <etienne.lanzeray@gmail.com>
2024-06-10 00:40:58 +0200

    modified:   environment.py

--- environment.py
+++ environment.py
@@ -70,9 +70,9 @@ class CustomEnv(gym.Env):
     def step(self, action):
         self.current_step += 1
         x_pos, y_pos, radius = action
-        x_pos = np.clip(x_pos * self.target.shape[1], 0, self.target.shape[1] - 1)
-        y_pos = np.clip(y_pos * self.target.shape[0], 0, self.target.shape[0] - 1)
-        radius = np.clip(1 + radius * min(self.target.shape[:2]) / 2, 1, min(self.target.shape[:2]) / 2)
+        x_pos = x_pos * self.target.shape[1]
+        y_pos = y_pos * self.target.shape[0]
+        radius = 1 + radius * min(self.target.shape[:2]) / 2, 1, min(self.target.shape[:2]) / 2
         # print("x, y, r : ", x_pos, y_pos, radius)
 
         x_pos = np.array([x_pos])
