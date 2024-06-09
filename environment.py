272865ed84c0ad0d54f1bd4d292211dbb920d2ed
EtiNL <etienne.lanzeray@gmail.com>
2024-06-10 01:43:12 +0200

    modified:   environment.py

--- environment.py
+++ environment.py
@@ -81,9 +81,10 @@ class CustomEnv(gym.Env):
             next_state = np.sum(np.abs(self.target - self.toile), axis=2) / self.init_state_divisor
             current_loss = loss(self.target, self.toile, self.semaphore)
             absolute_reward = self.previous_loss - current_loss
-            proportional_reward = max(0, 100*self.target_size*(self.previous_loss - current_loss)/(np.pi*int(radius)**2))
+            proportional_reward = max(0, self.target_size*(self.previous_loss - current_loss)/(np.pi*int(radius)**2))
             reward = absolute_reward + proportional_reward
-            wandb.log({"proportional reward" : np.log10(1+proportional_reward)})
+            wandb.log({"proportional reward" : proportional_reward,
+                       "rayon": int(radius)})
             self.previous_loss = current_loss
             done = self.current_step >= 10000 or current_loss <= 0.1 * self.init_loss
             if done:
